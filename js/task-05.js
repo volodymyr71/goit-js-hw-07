d1470841938678c79e25fa3e248be33914fd983d
Volodymyr Kyrylenko <kiril2727@gmail.com>
2020-11-16 00:40:45 +0200

    task-7
    початок роботи з задачкою 7

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,13 @@
 // Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
 // подставляет его текущее значение в span#name-output. 
 // Если инпут пустой, в спане должна отображаться строка 'незнакомец'
+
+// <input type="text" placeholder="Ваше имя?" id="name-input" />
+//       <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
+
+const inputName = document.querySelector('#name-input'); 
+const outputName = document.querySelector('#name-output'); 
+inputName.addEventListener('input', inputText => {
+    outputName.textContent = inputText.target.value
+    if (inputText.target.value === "") {outputName.textContent = 'незнакомец'}
+});
